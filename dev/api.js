c821c9fb5a2b99288f61aec1adf4b011ca6c82e4
Eric Traub <est2114@columbia.edu>
2018-04-18 19:04:24 -0400

    record & build get /blockchain endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
- 
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
 
- 
+const bitcoin = new Blockchain();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// get entire blockchain
 app.get('/blockchain', function (req, res) {
-  
+	res.send(bitcoin);
 });
 
+
+// post a new transaction
 app.post('/transaction/new', function(req, res) {
-
+	res.send(`The amount was ${req.body.amount}`);
 });
 
+
+// mine a block
 app.get('/mine', function(req, res) {
 
 });
  
+
 
 
 app.listen(3000, function() {
