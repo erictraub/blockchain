21e9fabd50c75e74043a5207a890389d65e83c51
Eric Traub <est2114@columbia.edu>
2018-04-23 21:22:41 -0400

    build and record through register-and-broadcast-node endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const uuid = require('uuid/v1');
+const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid().split('-').join('');
 
@@ -49,13 +51,59 @@ app.get('/mine', function(req, res) {
 });
 
 
+// register a node and broadcast it the network
+app.post('/register-and-broadcast-node', function(req, res) {
+	const newNodeUrl = req.body.newNodeUrl;
+	if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) bitcoin.networkNodes.push(newNodeUrl);
+
+	const regNodesPromises = [];
+	bitcoin.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/register-node',
+			method: 'POST',
+			body: { newNodeUrl: newNodeUrl },
+			json: true
+		};
+
+		regNodesPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(regNodesPromises)
+	.then(data => {
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+			method: 'POST',
+			body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+			json: true
+		};
+
+		return rp(bulkRegisterOptions);
+	})
+	.then(data => {
+		res.json({ note: 'New node registered with network successfully.' });
+	});
+});
+
+
+// register a node with the network
+app.post('/register-node', function(req, res) {
+	// PUT THE IF STATEMENT IN LINE 68 FIRST !!!!
+});
+
+
+// register multiple nodes at once
+app.post('/register-nodes-bulk', function(req, res) {
+
+});
 
 
 
 
 
-app.listen(3000, function() {
-	console.log('Listening on port 3000...');
+
+
+app.listen(port, function() {
+	console.log(`Listening on port ${port}...`);
 });
 
 
